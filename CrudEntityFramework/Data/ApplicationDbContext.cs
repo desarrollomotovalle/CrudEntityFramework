253eb6836343f0ed7a7685f3eac41d8f9d70f63e
Cesar Augusto Ariza <cariza@MotCom.com>
2021-05-21 17:12:04 -0500

    Instalación paquetes Nugets y primer migración

---
+++ CrudEntityFramework/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudEntityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CrudEntityFramework.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+                            
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
--- CrudEntityFramework/Models/Usuario.cs
+++ CrudEntityFramework/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace CrudEntityFramework.Models
 {
     public class Usuario
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name ="Teléfono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El Celular es obligatorio")]
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "El Correo es obligatorio")]
+        public string Email { get; set; }
     }
 }
