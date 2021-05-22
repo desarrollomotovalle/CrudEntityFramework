b5f5102d7dab37e1b1031884116fe5f0125d3e04
Cesar Augusto Ariza <cariza@MotCom.com>
2021-05-21 21:15:56 -0500

    Agregar eliminar y finalizar proyecto

--- CrudEntityFramework/Controllers/HomeController.cs
+++ CrudEntityFramework/Controllers/HomeController.cs
@@ -49,15 +49,16 @@ namespace CrudEntityFramework.Controllers
         [HttpGet]
         public IActionResult Edit(int? id)
         {
-            if (id==null)
+            if (id == null)
             {
                 return NotFound();
             }
 
             var usuario = _context.Usuario.Find(id);
 
-            if(usuario == null){
-                return NotFound();   
+            if (usuario == null)
+            {
+                return NotFound();
             }
 
             return View(usuario);
@@ -93,6 +94,40 @@ namespace CrudEntityFramework.Controllers
             }
 
             return View(usuario);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuario.Find(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteRegistro(int? id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return View();
+            }
+
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index)); 
         }
 
     }
